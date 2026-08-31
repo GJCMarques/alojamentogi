7a09015533991108e3e4c73b704d0b626ba31ec0
GJCMarques <105648283+GJCMarques@users.noreply.github.com>
2026-08-31 16:35:46 +0100

    Permite leitura dinâmica da base de dados via variáveis de ambiente

--- config/config.php
+++ config/config.php
@@ -3,12 +3,12 @@
 return [
 
     'db' => [
-        'host' => '127.0.0.1',
-        'name' => 'acasadogi',
-        'user' => 'root',
-        'pass' => '',
+        'host' => getenv('DB_HOST') ?: '127.0.0.1',
+        'name' => getenv('DB_NAME') ?: 'acasadogi',
+        'user' => getenv('DB_USER') ?: 'root',
+        'pass' => getenv('DB_PASS') ?: '',
         'charset' => 'utf8mb4',
-        'port' => 3306
+        'port' => getenv('DB_PORT') ?: 3306
     ],
 
     'app' => [
